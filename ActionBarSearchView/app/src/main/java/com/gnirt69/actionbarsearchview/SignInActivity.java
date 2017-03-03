537998b45b993d99f7a2bf41cd8da8dadf0d0a29
Minh T. Vu <minhtvu@mymail.mines.edu>
2017-03-03 15:49:41 -0700

    Views added

---
+++ ActionBarSearchView/app/src/main/java/com/gnirt69/actionbarsearchview/SignInActivity.java
@@ -0,0 +1,11 @@
+package com.gnirt69.actionbarsearchview;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by minhtvu on 3/3/17.
+ */
+
+public class SignInActivity extends AppCompatActivity {
+    
+}
