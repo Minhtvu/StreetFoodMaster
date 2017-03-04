8c025d6e2ea73da0c10b4abb0228f49377a8d900
Minh T. Vu <minhtvu@mymail.mines.edu>
2017-03-03 23:51:29 -0700

    Refactor the project

---
+++ app/src/main/java/com/gnirt69/StreetFoodMaster/ProfileActivity.java
@@ -0,0 +1,35 @@
+package com.gnirt69.StreetFoodMaster;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by minhtvu on 3/3/17.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+    private Button menuButton;
+    private Button faveButton;
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity);
+        menuButton = (Button) findViewById(R.id.menu_button);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Menu of this shop will show up", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        faveButton = (Button) findViewById(R.id.fave_button);
+        faveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Favorite food of this shop will be suggested", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/gnirt69/StreetFoodMaster/SurpriseActivity.java
@@ -0,0 +1,23 @@
+package com.gnirt69.StreetFoodMaster;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by minhtvu on 3/3/17.
+ */
+
+public class SurpriseActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.surprise_activity);
+        Button back = (Button) findViewById(R.id.back_button);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
