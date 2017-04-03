52df6bf81a34ae4a87c82e00f53f9c0947ea069b
Minh T. Vu <minhtvu@mymail.mines.edu>
2017-04-02 22:39:34 -0600

    Add current location tracking

--- app/src/main/java/com/gnirt69/StreetFoodMaster/OptionActivity.java
+++ app/src/main/java/com/gnirt69/StreetFoodMaster/OptionActivity.java
@@ -1,10 +1,21 @@
 package com.gnirt69.StreetFoodMaster;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -16,6 +27,9 @@ public class OptionActivity extends AppCompatActivity {
     private Button streetButton;
     private Button foodButton;
     private Button signInButton;
+    private double latPoint;
+    private double lngPoint;
+    private final int REQUEST_PERMISSION_ACCESS_FINE_LOCATION = 1337;
     private static final String TAG = "OptionActivity";
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -24,12 +38,16 @@ public class OptionActivity extends AppCompatActivity {
         currentButton = (Button) findViewById(R.id.search_location_button);
         currentButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), SearchActivity.class);
-                startActivity(intent);
+                Location current = getLastBestLocation();
+                latPoint = current.getLatitude();
+                lngPoint = current.getLongitude();
+                Log.i(TAG, "LatPoint: " + latPoint);
+                Log.i(TAG, "LngPoint: " + lngPoint);
+
             }
         });
-        foodButton = (Button) findViewById(R.id.search_street_button);
-        foodButton.setOnClickListener(new View.OnClickListener() {
+        streetButton = (Button) findViewById(R.id.search_street_button);
+        streetButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), SearchActivity.class);
                 startActivity(intent);
@@ -49,6 +67,84 @@ public class OptionActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    private Location getLastBestLocation() {
+        showLocationPermission();
+        LocationManager mLocationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        try{
+        Location locationGPS = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location locationNet = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            long GPSLocationTime = 0;
+            if (null != locationGPS) { GPSLocationTime = locationGPS.getTime(); }
+
+            long NetLocationTime = 0;
+
+            if (null != locationNet) {
+                NetLocationTime = locationNet.getTime();
+            }
+
+            if ( 0 < GPSLocationTime - NetLocationTime ) {
+                Log.i(TAG, "Here: " + latPoint);
+                return locationGPS;
+            }
+            else {
+                Log.i(TAG, "Here1: " + latPoint);
+                return locationNet;
+            }}
+        catch(SecurityException e)
+        {
+            Log.i(TAG,"Exception!!!");
+        }
+        return null;
+    }
+    private void showLocationPermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                showExplanation("Permission Needed", "Rationale", Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_PERMISSION_ACCESS_FINE_LOCATION);
+            } else {
+                requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_PERMISSION_ACCESS_FINE_LOCATION);
+            }
+        } else {
+            Toast.makeText(this, "Permission (already) Granted!", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            String permissions[],
+            int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_ACCESS_FINE_LOCATION:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    private void showExplanation(String title,
+                                 String message,
+                                 final String permission,
+                                 final int permissionRequestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        requestPermission(permission, permissionRequestCode);
+                    }
+                });
+        builder.create().show();
+    }
+
+    private void requestPermission(String permissionName, int permissionRequestCode) {
+        ActivityCompat.requestPermissions(this,
+                new String[]{permissionName}, permissionRequestCode);
     }
     /*
     @Override
