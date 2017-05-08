036fab690637459fca43a276cb69be56e78d4d06
RJ Gibson <rgibson@mymail.mines.edu>
2017-05-07 18:06:16 -0600

    Fixed Merge Errors.

--- app/src/main/java/com/gnirt69/StreetFoodMaster/MainActivity.java
+++ app/src/main/java/com/gnirt69/StreetFoodMaster/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
